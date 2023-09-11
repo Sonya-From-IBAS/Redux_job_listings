7a79ca06b62747276a20fc73d3667e3812a4a442
Leonid <Anevskyi2018@yandex.ru>
2023-09-11 23:29:09 +0400

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { TheHeader } from './components/TheHeader';
+import { FilterPanel } from './components/FilterPanel';
+import { JobList } from './components/JobList';
+
+function App() {
+    return (
+        <>
+            <TheHeader />
+            <div className="container">
+                <FilterPanel />
+                <JobList />
+            </div>
+        </>
+    );
+}
+
+export default App;
